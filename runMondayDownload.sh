43e7e01b71fe714973c81e5ed1720642552074f6
HappyHarry1130 <andril.com@outlook.com>
2024-06-09 14:52:45 -0700

    update

--- runMondayDownload.sh
+++ runMondayDownload.sh
@@ -6,7 +6,7 @@ DL_DIR_NAME=monday_downloader
 DL_BQ_DATASET_NAME=Monday
 DL_BQ_TABLE_NAME=Monday
 DL_CSV_DIR_NAME=output
-DL_GS_BUCKET_NAME=gs://statbid1/statbid-argos-datacollection-prod/knifecenter/monday
+DL_GS_BUCKET_NAME=gs://statbid-argos-datacollection-prod1/123inkjets/monday
 
 gcloud auth activate-service-account --key-file=../feed-storage-94d94e70e271.json
 
